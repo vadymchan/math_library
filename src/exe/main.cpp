204f445a0a835e717064b4f06aa46948b82b6df3
vadymchan <vadymchan@gmail.com>
2023-07-03 23:53:12 +0300

    finally added google benchmark (https://github.com/google/benchmark.git)

--- src/exe/main.cpp
+++ src/exe/main.cpp
@@ -16,65 +16,36 @@
 //}
 
 
-//-------------------------------------------------------------
-
+#include <gtest/gtest.h>
 #include <benchmark/benchmark.h>
 
-// The function to be benchmarked
-static void MyBenchmarkFunction(benchmark::State& state) {
-    // Perform setup here
-    for (auto _ : state) {
-        // Code to be benchmarked
-    }
+// Function to test
+int add(int a, int b) {
+    return a + b;
 }
 
-// Register the benchmark
-BENCHMARK(MyBenchmarkFunction);
+// Google Test
+TEST(AddTest, HandlesPositiveInput) {
+    ASSERT_EQ(6, add(2, 4));
+}
 
-// Optionally, define custom arguments for the benchmark
-// BENCHMARK(MyBenchmarkFunction)->Arg(10)->Arg(100)->Arg(1000);
+// Google Benchmark
+static void BM_Add(benchmark::State& state) {
+    for (auto _ : state)
+        add(2, 4);
+}
+// Register the function as a benchmark
+BENCHMARK(BM_Add);
 
-// Define the main function
+// Main function
 int main(int argc, char** argv) {
-    // Initialize the benchmark
-    benchmark::Initialize(&argc, argv);
-
-    // Run the benchmark
-    benchmark::RunSpecifiedBenchmarks();
-
-    return 0;
+    ::testing::InitGoogleTest(&argc, argv);
+    ::benchmark::Initialize(&argc, argv);
+    // Run the tests
+    int test_result = RUN_ALL_TESTS();
+    // If tests are successful, run the benchmarks
+    if (test_result == 0) {
+        ::benchmark::RunSpecifiedBenchmarks();
+    }
+    return test_result;
 }
-
-
-//-------------------------------------------------------------
-//
-//#include <gtest/gtest.h>
-//
-//// Example class to test
-//class MyClass {
-//public:
-//    int Add(int a, int b) {
-//        return a + b;
-//    }
-//};
-//
-//// Test fixture
-//class MyTestClass : public testing::Test {
-//protected:
-//    MyClass myClass;
-//};
-//
-//// Test case
-//TEST_F(MyTestClass, AddTest) {
-//    int result = myClass.Add(2, 3);
-//    EXPECT_EQ(result, 5);
-//}
-//
-//// Main function
-//int main(int argc, char** argv) {
-//    // Initialize Google Test
-//    ::testing::InitGoogleTest(&argc, argv);
-//
-//    // Run all tests
-//    return RUN_ALL_TESTS();
-//}
