ce7327f6e128e4598ea28b875428e389a50f7e0f
vadymchan <vadymchan@gmail.com>
2023-07-05 22:06:42 +0300

    made matrix class template. Added Options for choosing element layout (row-major / column-major)

--- include/Matrix.h
+++ include/Matrix.h
@@ -1,23 +1,24 @@
 #pragma once
 
+#include "Options.h"
 
 namespace math
 {
+	template<typename T, unsigned int Rows, unsigned int Columns, Options Option>
 	class Matrix
 	{
 	public:
-		Matrix();
-		~Matrix();
+		Matrix() : m_data_(new T[Rows * Columns]) {}
+		
+		~Matrix()
+		{
+			delete[] m_data_;
+		}
+
+		
 
 	private:
-
+		T* m_data_;
 	};
 
-	Matrix::Matrix()
-	{
-	}
-
-	Matrix::~Matrix()
-	{
-	}
 }
