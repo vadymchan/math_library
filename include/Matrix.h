e7649e7b13ce5893b6cc46135d1d824eea5d682e
vadymchan <vadymchan@gmail.com>
2023-07-06 22:34:50 +0300

    - added checking for allocation on stack or heap - added access element (for modification and for read-only)

--- include/Matrix.h
+++ include/Matrix.h
@@ -1,24 +1,66 @@
 #pragma once
 
+#include <type_traits>
+#include <cassert>
 #include "Options.h"
+
+constexpr unsigned int STACK_ALLOCATION_LIMIT = 16;
 
 namespace math
 {
-	template<typename T, unsigned int Rows, unsigned int Columns, Options Option>
+	template<typename T, unsigned int Rows, unsigned int Columns, Options Option = Options::ROW_MAJOR>
 	class Matrix
 	{
 	public:
-		Matrix() : m_data_(new T[Rows * Columns]) {}
-		
-		~Matrix()
+		Matrix()
 		{
-			delete[] m_data_;
+			if constexpr (UseHeap) {
+				m_data_ = new T[Rows * Columns];
+			}
 		}
 
-		
+		~Matrix()
+		{
+			if constexpr (UseHeap) {
+				delete[] m_data_;
+			}
+		}
+
+		T& operator()(unsigned int row, unsigned int col)
+		{
+			if constexpr (Option == Options::ROW_MAJOR) {
+				return m_data_[row * Columns + col];
+			}
+			else {
+				return m_data_[col * Rows + row];
+			}
+		}
+
+		const T& operator()(unsigned int row, unsigned int col) const
+		{
+			if constexpr (Option == Options::ROW_MAJOR) {
+				return m_data_[row * Columns + col];
+			}
+			else {
+				return m_data_[col * Rows + row];
+			}
+		}
+
+		const T& coeff(unsigned int row, unsigned int col) const
+		{
+			assert(row < Rows && col < Columns && "Index out of bounds");
+			return operator()(row, col);
+		}
+
+		T& coeffRef(unsigned int row, unsigned int col)
+		{
+			assert(row < Rows && col < Columns && "Index out of bounds");
+			return operator()(row, col);
+		}
 
 	private:
-		T* m_data_;
+		static const bool UseHeap = sizeof(T) * Rows * Columns > STACK_ALLOCATION_LIMIT;
+		using DataType = typename std::conditional<UseHeap, T*, T[Rows * Columns]>::type;
+		DataType m_data_;
 	};
-
 }
