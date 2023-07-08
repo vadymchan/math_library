5d4faba2f9ffcdc4edf4b20f647cdb05a4599439
vadymchan <vadymchan@gmail.com>
2023-07-08 22:54:06 +0300

    added hangle for the case when the elements of array not fit to the AVX instruction (currently naive and not optimised)

--- include/InstructionSet.h
+++ include/InstructionSet.h
@@ -55,13 +55,22 @@ private:
 
 	static void add_avx2(float* a, const float* b, size_t size)
 	{
-		for (size_t i = 0; i < size; i += AVX_SIMD_WIDTH) {
+		size_t aligned_size = (size / AVX_SIMD_WIDTH) * AVX_SIMD_WIDTH;
+		size_t i = 0;
+
+		for (i = 0; i < aligned_size; i += AVX_SIMD_WIDTH) {
 			__m256 ymm1 = _mm256_loadu_ps(a + i);
 			__m256 ymm2 = _mm256_loadu_ps(b + i);
 			ymm1 = _mm256_add_ps(ymm1, ymm2);
 			_mm256_storeu_ps(a + i, ymm1);
 		}
+
+		// Handling remaining elements
+		for (; i < size; ++i) {
+			a[i] += b[i];
+		}
 	}
+
 
 	static void add_avx(float* a, const float* b, size_t size)
 	{
@@ -85,10 +94,16 @@ private:
 	static void add_scalar_avx2(float* a, float scalar, size_t size)
 	{
 		__m256 ymm0 = _mm256_set1_ps(scalar);
-		for (size_t i = 0; i < size; i += AVX_SIMD_WIDTH) {
+		size_t i = 0;
+
+		for (; i < size; i += AVX_SIMD_WIDTH) {
 			__m256 ymm1 = _mm256_loadu_ps(a + i);
 			ymm1 = _mm256_add_ps(ymm1, ymm0);
 			_mm256_storeu_ps(a + i, ymm1);
+		}
+
+		for (; i < size; ++i) {
+			a[i] += scalar;
 		}
 	}
 
