a9741bfa7919a4cbce9843473109dab49c0f3bca
vadymchan <vadymchan@gmail.com>
2023-07-28 21:18:39 +0300

    added doxygen comments into the Options

--- src/lib/options/Options.h
+++ src/lib/options/Options.h
@@ -1,16 +1,24 @@
 /**
  * @file Options.h
+ * @brief This file contains the Options enum used for specifying various configuration options for mathematical objects.
  */
-
 
 #pragma once
 
 namespace math
 {
-	enum class Options
-	{
-		COLUMN_MAJOR,
-		ROW_MAJOR,
-		COUNT
-	};
+    /**
+     * @enum Options
+     * @brief Enum class for specifying various configuration options for mathematical objects.
+     *
+     * This enum is used to specify different configuration options for mathematical objects such as matrices and vectors.
+     * For instance, it can be used to specify the memory layout (row-major or column-major) of a Matrix object.
+     * This can affect performance for certain operations.
+     */
+    enum class Options
+    {
+        COLUMN_MAJOR, /**< Enum value for column-major order. In this layout, the elements in the same column of a matrix are stored contiguously in memory. */
+        ROW_MAJOR,    /**< Enum value for row-major order. In this layout, the elements in the same row of a matrix are stored contiguously in memory. */
+        COUNT         /**< Enum value for counting the number of options.  */
+    };
 }
