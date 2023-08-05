9ae07669f052ef81d13275e038e6910d55fe16c1
vadymchan <vadymchan@gmail.com>
2023-08-05 21:55:38 +0300

    added placeholders for view matrix and projection matrix (both orho and perspective)

---
+++ include/Graphics.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "Matrix.h"
+
+namespace math
+{
+
+	//lookAtLH(eyePosition, centerPosition, upDirection)
+	//lookAtLH(eyePosition, centerPosition, upDirection)
+	//perspectiveFovRH(fov, aspectRatio, nearPlane, farPlane)
+	//perspectiveFovLH(fov, aspectRatio, nearPlane, farPlane)
+	//perspectiveOffCenter(left, right, bottom, top, nearPlane, farPlane)
+	//orthoRH(left, right, bottom, top, nearPlane, farPlane)
+	//orthoLH(left, right, bottom, top, nearPlane, farPlane)
+
+} // namespace math
