2f1b419e6f680bc05aabaff2db555c044bf5a7ed
vadymchan <vadymchan@gmail.com>
2023-11-19 18:20:17 +0200

    define vector class. add underlying type (matrix) to vector

--- include/Vector.h
+++ include/Vector.h
@@ -10,9 +10,9 @@
 
 namespace math {
 
-// Vector of floats
-template <unsigned int Size, Options Option = Options::RowMajor>
-using VectorXf = Matrix<float, Size, 1, Option>;
+template <typename T, unsigned int Size, Options Option = Options::RowMajor>
+class Vector {
+  public:
 
 // Vector of doubles
 template <unsigned int Size, Options Option = Options::RowMajor>
@@ -42,19 +42,13 @@ using RowVector2f = RowVectorXf<2>;
 using RowVector3f = RowVectorXf<3>;
 using RowVector4f = RowVectorXf<4>;
 
-using Vector2d    = VectorXd<2>;
-using Vector3d    = VectorXd<3>;
-using Vector4d    = VectorXd<4>;
-using RowVector2d = RowVectorXd<2>;
-using RowVector3d = RowVectorXd<3>;
-using RowVector4d = RowVectorXd<4>;
+  private:
+  using UnderlyingType = std::conditional_t<Option == Options::RowMajor,
+                                            Matrix<T, Size, 1, Option>,
+                                            Matrix<T, 1, Size, Option>>;
 
-using Vector2i    = VectorXi<2>;
-using Vector3i    = VectorXi<3>;
-using Vector4i    = VectorXi<4>;
-using RowVector2i = RowVectorXi<2>;
-using RowVector3i = RowVectorXi<3>;
-using RowVector4i = RowVectorXi<4>;
+  UnderlyingType m_dataStorage_;
+};
 
 }  // namespace math
 
