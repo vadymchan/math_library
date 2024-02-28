8de01f4c648b2028b43629926d14f9ab2a984664
vadymchan <vadymchan@gmail.com>
2024-02-28 22:55:16 +0200

    minor change (rearrange headers)

--- include/math_library/all.h
+++ include/math_library/all.h
@@ -5,10 +5,10 @@
 #ifndef MATH_LIBRARY_ALL_H
 #define MATH_LIBRARY_ALL_H
 
+#include "dimension.h"
 #include "graphics.h"
 #include "matrix.h"
-#include "vector.h"
 #include "point.h"
-#include "dimension.h"
+#include "vector.h"
 
 #endif
