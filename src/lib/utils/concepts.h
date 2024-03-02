eae229d4182e92076ea03f00cdc22346823b08f5
vadymchan <vadymchan@gmail.com>
2024-03-02 14:38:25 +0200

    refactor code (remove unused concept)

--- src/lib/utils/concepts.h
+++ src/lib/utils/concepts.h
@@ -19,9 +19,6 @@ template <unsigned int Rows, unsigned int Columns>
 concept OneDimensional = (Rows == 1 || Columns == 1);
 
 template <typename U, typename... Args>
-concept AllSameAs = (std::same_as<Args, U> && ...);
-
-template <typename U, typename... Args>
 concept AllConvertibleTo = (std::convertible_to<Args, U> && ...);
 
 template <auto Count, typename... Args>
