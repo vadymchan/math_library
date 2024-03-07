3d6b831d3cd6cba5545d01020de6feaa22ccccbe
vadymchan <vadymchan@gmail.com>
2024-03-07 21:34:01 +0200

    add utility functions / variable for degree / radians convertion

--- src/lib/utils/utils.h
+++ src/lib/utils/utils.h
@@ -7,6 +7,22 @@
 
 namespace math {
 
+static constexpr float g_kPi = 3.141592653f;
+
+template <typename T>
+  requires std::floating_point<T>
+constexpr auto g_radianToDegree(T radian) -> T {
+  constexpr T kToDegree = static_cast<T>(180) / g_kPi;
+  return radian * kToDegree;
+}
+
+template <typename T>
+  requires std::floating_point<T>
+constexpr auto g_degreeToRadian(T degree) -> T {
+  constexpr T kToRadian = g_kPi / static_cast<T>(180);
+  return degree * kToRadian;
+}
+
 template <typename T>
 auto g_abs(T value) -> T {
   if constexpr (std::is_integral_v<T>) {
