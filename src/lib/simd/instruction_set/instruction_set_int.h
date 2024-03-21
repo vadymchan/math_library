e1896fd34c36dc1f2918d931fcc0dca325f082a9
vadymchan <vadymchan@gmail.com>
2024-03-21 16:14:20 +0200

    add comparison two arrays functionality to int type specialization

--- src/lib/simd/instruction_set/instruction_set_int.h
+++ src/lib/simd/instruction_set/instruction_set_int.h
@@ -180,6 +180,26 @@ class InstructionSet<int> {
 #endif
   }
 
+   using CmpFunc = int (*)(const int*, const int*, size_t);
+
+  static auto GetCmpFunc() -> CmpFunc {
+#ifdef SUPPORTS_AVX2
+    return CmpAvx2;
+#elif defined(SUPPORTS_AVX)
+    return CmpAvx;
+#elif defined(SUPPORTS_SSE4_2)
+    return CmpSse42;
+#elif defined(SUPPORTS_SSE4_1)
+    return CmpSse41;
+#elif defined(SUPPORTS_SSSE3)
+    return CmpSsse3;
+#elif defined(SUPPORTS_SSE3)
+    return CmpSse3;
+#else
+    return CmpFallback;
+#endif
+  }
+
   private:
   static constexpr size_t s_kAvxSimdWidth = sizeof(__m256i) / sizeof(int);  // 8
   static constexpr size_t s_kSseSimdWidth = sizeof(__m128i) / sizeof(int);  // 4
@@ -921,6 +941,109 @@ class InstructionSet<int> {
   }
 
   // END: division scalar
+  //----------------------------------------------------------------------------
+
+    // BEGIN: comparison array
+  //----------------------------------------------------------------------------
+
+  static int CmpAvx2(const int* a, const int* b, size_t size) {
+    return CmpAvx(a, b, size);
+  }
+
+  static int CmpAvx(const int* a, const int* b, size_t size) {
+    const size_t kAvxLimit = size - (size % s_kAvxSimdWidth);
+    size_t       i         = 0;
+
+    for (; i < kAvxLimit; i += s_kAvxSimdWidth) {
+      __m256i aVec      = _mm256_loadu_si256((__m256i*)(a + i));
+      __m256i bVec      = _mm256_loadu_si256((__m256i*)(b + i));
+      __m256i cmpResult = _mm256_cmpgt_epi32(bVec, aVec);
+      int     mask      = _mm256_movemask_ps(_mm256_castsi256_ps(cmpResult));
+      if (mask != 0) {
+        return -1;
+      }
+      cmpResult = _mm256_cmpgt_epi32(aVec, bVec);
+      mask      = _mm256_movemask_ps(_mm256_castsi256_ps(cmpResult));
+      if (mask != 0) {
+        return 1;
+      }
+      cmpResult = _mm256_cmpeq_epi32(aVec, bVec);
+      mask      = _mm256_movemask_ps(_mm256_castsi256_ps(cmpResult));
+      if (mask == 0xFF) {
+        return 0;
+      }
+    }
+
+    // Handle any remainder
+    for (; i < size; ++i) {
+      if (a[i] < b[i]) {
+        return -1;
+      } else if (a[i] > b[i]) {
+        return 1;
+      }
+    }
+    return 0;
+  }
+
+  static int CmpSse42(const int* a, const int* b, size_t size) {
+    return CmpSse3(a, b, size);
+  }
+
+  static int CmpSse41(const int* a, const int* b, size_t size) {
+    return CmpSse3(a, b, size);
+  }
+
+  static int CmpSsse3(const int* a, const int* b, size_t size) {
+    return CmpSse3(a, b, size);
+  }
+
+  static int CmpSse3(const int* a, const int* b, size_t size) {
+    const size_t kSseLimit = size - (size % s_kSseSimdWidth);
+    size_t       i         = 0;
+
+    for (; i < kSseLimit; i += s_kSseSimdWidth) {
+      __m128i aVec      = _mm_loadu_si128((__m128i*)(a + i));
+      __m128i bVec      = _mm_loadu_si128((__m128i*)(b + i));
+      __m128i cmpResult = _mm_cmplt_epi32(aVec, bVec);
+      int     mask      = _mm_movemask_ps(_mm_castsi128_ps(cmpResult));
+      if (mask != 0) {
+        return -1;
+      }
+      cmpResult = _mm_cmpgt_epi32(aVec, bVec);
+      mask      = _mm_movemask_ps(_mm_castsi128_ps(cmpResult));
+      if (mask != 0) {
+        return 1;
+      }
+      cmpResult = _mm_cmpeq_epi32(aVec, bVec);
+      mask      = _mm_movemask_ps(_mm_castsi128_ps(cmpResult));
+      if (mask == 0xF) {
+        return 0;
+      }
+    }
+
+    // Handle any remainder
+    for (; i < size; ++i) {
+      if (a[i] < b[i]) {
+        return -1;
+      } else if (a[i] > b[i]) {
+        return 1;
+      }
+    }
+    return 0;
+  }
+
+  static int CmpFallback(const int* a, const int* b, size_t size) {
+    for (size_t i = 0; i < size; ++i) {
+      if (a[i] < b[i]) {
+        return -1;
+      } else if (a[i] > b[i]) {
+        return 1;
+      }
+    }
+    return 0;
+  }
+
+  // END: comparison array
   //----------------------------------------------------------------------------
 };
 
